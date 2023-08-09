edf2a71dc5653d876fb2c743a3f40fffeb135797
Habib Al Hasan Himel <djstar.himel@gmail.com>
2023-08-09 22:11:48 +0600

    updated

--- script/leaflet.js
+++ script/leaflet.js
@@ -4,213 +4,297 @@ document.addEventListener('DOMContentLoaded', function () {
     const showMapBtn = document.getElementById('showMapBtn');
     var showListBtn;
     var propertyListVisible = false;
-    const arizonaCoordinates = [40.7310749,-73.5169878];
+
 
     let map = null;
     let markers = []; // Store markers to later toggle their visibility
 
     showMapBtn.addEventListener('click', function () {
-        contentContainer.className= 'd-none';
-        
-        if (map !== null) {
-
-          
-            mapContainer.className='d-block'
-
-            map.remove(); // Remove the previous map
-            map = null;
-            markers = []; // Reset markers array
-        }
-
-        var searchInput = document.getElementById('searchInput').value;
-        var priceRangeInput = document.getElementById('priceInput').value;
-
-        // Function to update the map and list based on search criteria
-        function updateMapAndList() {
-            // Clear property list container
-            const propertyListContainer = document.getElementById('propertyListContainer');
-            propertyListContainer.innerHTML = '';
-
-            const searchPropertyList = {
-                'Hawa Mahal,USA': [
-                    {
-                        place: 'Peabody, MA, USA',
-                        coordinates: [41.3948,-73.4540],
-                        price: 150
-                    },
-                    {
-                        place: 'Northampton, MA, USA',
-                        coordinates: [41.4080,-75.6624],
-                        price: 200
-                    },
-                    {
-                        place: 'Newton, MA, USA',
-                        coordinates: [40.6084,-75.4902],
-                        price: 400
-                    },
-                    {
-                        place: 'Newburyport, MA, USA',
-                        coordinates: [41.6362,-70.9342],
-                        price: 600
-                    },
-                    {
-                        place: 'New Bedford, MA, USA',
-                        coordinates: [39.2904,-76.6122],
-                        price: 700
-                    },
-                    {
-                        place: 'Medford, MA, USA',
-                        coordinates: [40.2737,-76.8844],
-                        price: 300
-                    },
-                    {
-                        place: 'Malden, MA, USA',
-                        coordinates: [40.2206,-74.7597],
-                        price: 450
-                    },
-                    {
-                        place: 'Leominster, MA, USA',
-                        coordinates: [39.9537,-74.1979],
-                        price: 350
-                    },
-                    // Add more properties for 'Hawa Mahal, USA'
-                ],
-
-                'Hawes,UK': [
-                    {
-                        place: 'Peabody, MA, UK',
-                        coordinates: [41.3948,-73.4540],
-                        price: 150
-                    },
-                    {
-                        place: 'Northampton, MA, UK',
-                        coordinates: [41.4080,-75.6624],
-                        price: 200
-                    },
-                    {
-                        place: 'Newton, MA, UK',
-                        coordinates: [40.6084,-75.4902],
-                        price: 400
-                    },
-                    {
-                        place: 'Newburyport, MA, UK',
-                        coordinates: [41.6362,-70.9342],
-                        price: 600
-                    },
-                    {
-                        place: 'New Bedford, MA, UK',
-                        coordinates: [39.2904,-76.6122],
-                        price: 700
-                    },
-                    {
-                        place: 'Medford, MA, UK',
-                        coordinates: [40.2737,-76.8844],
-                        price: 300
-                    },
-                    {
-                        place: 'Malden, MA, UK',
-                        coordinates: [40.2206,-74.7597],
-                        price: 450
-                    },
-                    {
-                        place: 'Leominster, MA, UK',
-                        coordinates: [39.9537,-74.1979],
-                        price: 350
-                    },
-                    // Add more properties for 'Hawa Mahal, USA'
-                ],
-                // Add more locations here
-            };
 
 
+        // Get the values from the input fields
+        let place = document.getElementById("searchInput").value;
+        let checkin = document.getElementById("check-in-input").value;
+        let checkout = document.getElementById("check-out-input").value;
+        let guest = document.getElementById("guestInput").value;
+        let priceRange = document.getElementById("priceInput").value;
 
 
-            // Get properties based on the search input
-            const selectedLocationData = searchPropertyList[searchInput] || [];
-            let filteredProperties = selectedLocationData;
+        if (place === '' || checkin === '' || checkout === '' || guest === '' || priceRange === '') {
+            alert('Kindly provide all the necessary values to display the map');
+            event.preventDefault();
+            return;
+        } else {
 
-            // Filter properties based on price range if a range is provided
-            if (priceRangeInput) {
-                const [minPrice, maxPrice] = priceRangeInput.split('-').map(parseFloat);
-                filteredProperties = selectedLocationData.filter(property => property.price >= minPrice && property.price <= maxPrice);
+            contentContainer.className = 'd-none';
+
+            if (map !== null) {
+
+
+                mapContainer.className = 'd-block'
+
+                map.remove(); // Remove the previous map
+                map = null;
+                markers = []; // Reset markers array
             }
 
+            var searchInput = document.getElementById('searchInput').value;
+            var priceRangeInput = document.getElementById('priceInput').value;
 
-            const coordinatesData = [
-                {
-                    place: 'Hawa Mahal,USA',
-                    coordinates: [41.3948,-73.4540]
-                },
-                {
-                    place: 'Hawes,UK',
-                    coordinates: [40.2206,-74.7597]
-                },
-                // Add more locations with coordinates
-            ];
-            
-            // Filter out the coordinates based on the search input
-            var filteredCoordinates = coordinatesData.filter(data => data.place === searchInput);
-            var coordinatesArray;
-            
-            // Extract the coordinated value as an array
+            // Function to update the map and list based on search criteria
+            function updateMapAndList() {
+                // Clear property list container
+                const propertyListContainer = document.getElementById('propertyListContainer');
+                propertyListContainer.innerHTML = '';
 
-            if(filteredCoordinates.length>0){
-               coordinatesArray  = filteredCoordinates[0].coordinates;
-            }
-            else{
-                coordinatesArray = [40.2206,-74.7597];
-            }
+                const searchPropertyList = {
+                    'Hawa Mahal,USA': [{
+                            place: 'Peabody, MA, USA',
+                            coordinates: [41.3948, -73.4540],
+                            price: 150
+                        },
+                        {
+                            place: 'Northampton, MA, USA',
+                            coordinates: [41.4080, -75.6624],
+                            price: 200
+                        },
+                        {
+                            place: 'Newton, MA, USA',
+                            coordinates: [40.6084, -75.4902],
+                            price: 400
+                        },
+                        {
+                            place: 'Newburyport, MA, USA',
+                            coordinates: [41.6362, -70.9342],
+                            price: 600
+                        },
+                        {
+                            place: 'New Bedford, MA, USA',
+                            coordinates: [39.2904, -76.6122],
+                            price: 700
+                        },
+                        {
+                            place: 'Medford, MA, USA',
+                            coordinates: [40.2737, -76.8844],
+                            price: 300
+                        },
+                        {
+                            place: 'Malden, MA, USA',
+                            coordinates: [40.2206, -74.7597],
+                            price: 450
+                        },
+                        {
+                            place: 'Leominster, MA, USA',
+                            coordinates: [39.9537, -74.1979],
+                            price: 350
+                        },
+                        // Add more properties for 'Hawa Mahal, USA'
+                    ],
 
-            
+                    'Hawes,UK': [{
+                            place: 'Peabody, MA, UK',
+                            coordinates: [40.7608, -111.8910],
+                            price: 150
+                        },
+                        {
+                            place: 'Northampton, MA, UK',
+                            coordinates: [40.6461, -111.4980],
+                            price: 200
+                        },
+                        {
+                            place: 'Newton, MA, UK',
+                            coordinates: [40.8894, -111.8808],
+                            price: 400
+                        },
+                        {
+                            place: 'Newburyport, MA, UK',
+                            coordinates: [41.0415, -111.6782],
+                            price: 600
+                        },
+                        {
+                            place: 'New Bedford, MA, UK',
+                            coordinates: [40.5308, -112.2983],
+                            price: 700
+                        },
+                        {
+                            place: 'Medford, MA, UK',
+                            coordinates: [40.5071, -111.4138],
+                            price: 300
+                        },
 
-           
+                    ],
 
-            // Create the map
-            map = L.map(mapContainer).setView(coordinatesArray, 7);
+                    'Hawthrone,CSA,USA': [{
+                            place: 'Peabody, MA, UK',
+                            coordinates: [32.7767, -96.7970],
+                            price: 150
+                        },
+                        {
+                            place: 'Northampton, MA, UK',
+                            coordinates: [32.3513, -95.3011],
+                            price: 200
+                        },
+                        {
+                            place: 'Newton, MA, UK',
+                            coordinates: [31.3382, -94.7292],
+                            price: 400
+                        },
+                        {
+                            place: 'Newburyport, MA, UK',
+                            coordinates: [31.4638, -100.4370],
+                            price: 600
+                        },
+                        {
+                            place: 'New Bedford, MA, UK',
+                            coordinates: [34.6035, -98.3959],
+                            price: 700
+                        },
+                        {
+                            place: 'Medford, MA, UK',
+                            coordinates: [35.4676, -97.5164],
+                            price: 300
+                        },
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
+                    ],
 
-            // Create a new "Show List" button on the map
-            showListBtn = L.control({
-                position: 'topright'
-            });
+                    'Hawaii,USA': [{
+                            place: 'Peabody, MA, UK',
+                            coordinates: [32.7767, -96.7970],
+                            price: 150
+                        },
+                        {
+                            place: 'Northampton, MA, UK',
+                            coordinates: [32.3513, -95.3011],
+                            price: 200
+                        },
+                        {
+                            place: 'Newton, MA, UK',
+                            coordinates: [31.3382, -94.7292],
+                            price: 400
+                        },
+                        {
+                            place: 'Newburyport, MA, UK',
+                            coordinates: [31.4638, -100.4370],
+                            price: 600
+                        },
+                        {
+                            place: 'New Bedford, MA, UK',
+                            coordinates: [34.6035, -98.3959],
+                            price: 700
+                        },
+                        {
+                            place: 'Medford, MA, UK',
+                            coordinates: [35.4676, -97.5164],
+                            price: 300
+                        },
 
-            showListBtn.onAdd = function () {
-                const div = L.DomUtil.create('div', 'show-list-button');
-                div.innerHTML = '<button class="btn btn-dark text-light btn-sm rounded-pill"><i class="fa-solid fa-list"></i> Show List</button>';
-                div.firstChild.addEventListener('click', function () {
-                    propertyListVisible = !propertyListVisible;
-                    togglePropertyList();
+                    ],
+
+                };
+
+
+
+
+                // Get properties based on the search input
+                const selectedLocationData = searchPropertyList[searchInput] || [];
+                let filteredProperties = selectedLocationData;
+
+                // Filter properties based on price range if a range is provided
+                if (priceRangeInput) {
+                    const [minPrice, maxPrice] = priceRangeInput.split('-').map(parseFloat);
+                    filteredProperties = selectedLocationData.filter(property => property.price >= minPrice && property.price <= maxPrice);
+                }
+
+
+                const coordinatesData = [{
+                        place: 'Hawa Mahal,USA',
+                        coordinates: [41.3948, -73.4540]
+                    },
+                    {
+                        place: 'Hawes,UK',
+                        coordinates: [40.7608, -111.8910]
+                    },
+                    {
+                        place: 'Hawthrone,CSA,USA',
+                        coordinates: [32.7767, -96.7970]
+                    },
+                    {
+                        place: 'Hawaii,USA',
+                        coordinates: [32.7767, -96.7970]
+                    },
+
+                    // Add more locations with coordinates
+                ];
+
+                // Filter out the coordinates based on the search input
+                var filteredCoordinates = coordinatesData.filter(data => data.place === searchInput);
+                var coordinatesArray;
+
+                // Extract the coordinated value as an array
+
+                if (filteredCoordinates.length > 0) {
+                    coordinatesArray = filteredCoordinates[0].coordinates;
+                } else {
+                    coordinatesArray = [40.2206, -74.7597];
+                }
+
+
+
+
+
+                // Create the map
+                map = L.map(mapContainer).setView(coordinatesArray, 7);
+
+                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(map);
+
+                // Create a new "Show List" button on the map
+                showListBtn = L.control({
+                    position: 'topright'
                 });
-                return div;
-            };
 
-            showListBtn.addTo(map);
+                showListBtn.onAdd = function () {
+                    const div = L.DomUtil.create('div', 'show-list-button');
+                    div.innerHTML = '<button class="btn btn-dark text-light btn-sm rounded-pill"><i class="fa-solid fa-list"></i> Show List</button>';
+                    div.firstChild.addEventListener('click', function () {
+                        propertyListVisible = !propertyListVisible;
+                        togglePropertyList();
+                    });
+                    return div;
+                };
 
-            // Add filtered properties to the property list container and create markers
-            filteredProperties.forEach(property => {
+                showListBtn.addTo(map);
 
-                const customIcon = L.divIcon({
-                    className: 'custom-icon',
-                    html: `<div class="custom-icon-content btn text-light btn-sm rounded-pill btn-dark">$${property.price}</div>`, // Replace property.customText with your custom text
-                    iconSize: [40, 40], // Adjust the size as needed
+                // Add filtered properties to the property list container and create markers
+                filteredProperties.forEach(property => {
+
+                    const customIcon = L.divIcon({
+                        className: 'custom-icon',
+                        html: `<div class="custom-icon-content btn text-light btn-sm rounded-pill btn-dark">$${property.price}</div>`, // Replace property.customText with your custom text
+                        iconSize: [40, 40], // Adjust the size as needed
+                    });
+
+                    // Add marker for the filtered property
+                    const marker = L.marker(property.coordinates, {
+                        icon: customIcon
+                    }).addTo(map);
+
+
                 });
-            
-                // Add marker for the filtered property
-                const marker = L.marker(property.coordinates,{icon: customIcon}).addTo(map);
-        
-               
-            });
+            }
+
+            // Function to toggle property list visibility
+            function togglePropertyList(properties) {
+                contentContainer.className = "container mt-4";
+                mapContainer.className = "d-none";
+            }
+
+            updateMapAndList();
+
         }
 
-        // Function to toggle property list visibility
-        function togglePropertyList(properties) {
-            contentContainer.className="container mt-4";
-            mapContainer.className="d-none";
-        }
 
-        updateMapAndList();
+
     });
 });
